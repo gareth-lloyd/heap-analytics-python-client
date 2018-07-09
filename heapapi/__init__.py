ed0998288a9a3ad63a40905785642b91725e1cf2
m-vdb <mvergerdelbove@work4labs.com>
2018-07-09 12:07:08 +0200

    cleanup code

--- heapapi/__init__.py
+++ heapapi/__init__.py
@@ -1,23 +1,29 @@
 import json
+
 import requests
 
-class HeapAPIClient():
+
+class HeapAPIClient:
+    """
+    The client for the Heap Api.
+    """
+    base_url = "https://heapanalytics.com/api"
+    headers = {'Content-Type': 'application/json'}
+
     def __init__(self, app_id):
         """
+        Initialize the client.
 
         :param app_id: Heap analytics app_id
         :type app_id: str
         """
-
-        self.base_url = "https://heapanalytics.com/api"
+        assert app_id, 'app_id must be valid!'
         self.app_id = app_id
 
-        if app_id is None:
-            raise RuntimeError("app_id must be valid!")
-
-    def track(self, identity=None, event=None, properties=None):
+    def track(self, identity, event, properties=None):
         """
-        Post a "track" event to the Heap Analytics API server
+        Send a "track" event to the Heap Analytics API server.
+
         :param identity: user identity
         :type identity: str
         :param event: event name
@@ -25,10 +31,6 @@ class HeapAPIClient():
         :param properties: optional, additional event properties
         :type properties: dict
         """
-
-        if identity is None or event is None:
-            raise RuntimeError("identity and event must be valid!")
-
         data = {
             "app_id": self.app_id,
             "identity": identity,
@@ -38,9 +40,14 @@ class HeapAPIClient():
         if properties is not None:
             data["properties"] = properties
 
-        requests.post(self.base_url + '/track', data=json.dumps(data), headers={'Content-Type': 'application/json'})
+        response = requests.post(
+            self.base_url + '/track',
+            data=json.dumps(data),
+            headers={'Content-Type': 'application/json'}
+        )
+        return response
 
-    def add_user_properties(self, identity=None, properties=None):
+    def add_user_properties(self, identity, properties=None):
         """
         Post a "add_user_properties" event to the Heap Analytics API server
         :param identity: user identity
@@ -48,10 +55,6 @@ class HeapAPIClient():
         :param properties: optional, additional properties to associate with the user
         :type properties: dict
         """
-
-        if identity is None:
-            raise RuntimeError("identity must be valid!")
-
         data = {
             "app_id": self.app_id,
             "identity": identity,
@@ -60,5 +63,9 @@ class HeapAPIClient():
         if properties is not None:
             data["properties"] = properties
 
-        requests.post(self.base_url + '/add_user_properties', data=json.dumps(data),
-                      headers={'Content-Type': 'application/json'})
+        response = requests.post(
+            self.base_url + '/add_user_properties',
+            data=json.dumps(data),
+            headers=self.headers
+        )
+        return response
